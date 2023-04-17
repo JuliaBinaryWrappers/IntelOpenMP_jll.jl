f52376b6d677192bf8cc6c65eaddc7d5d412d574
jlbuild <juliabuildbot@gmail.com>
2023-04-17 20:04:49 +0000

    IntelOpenMP_jll build 2023.1.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,7 +2,7 @@
 export libiomp
 
 JLLWrappers.@generate_wrapper_header("IntelOpenMP")
-JLLWrappers.@declare_library_product(libiomp, "libiomp5.dylib")
+JLLWrappers.@declare_library_product(libiomp, "@rpath/libiomp5.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
